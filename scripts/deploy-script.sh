39b8bf20d65f12ffd0537ddcd94961b853f38f23
esuau <evan.suau@etu.u-pec.fr>
2019-09-03 00:18:45 +0200

    ci(travis-ci): Login to Heroku registry before pushing image

--- scripts/deploy-script.sh
+++ scripts/deploy-script.sh
@@ -1,4 +1,5 @@
 curl https://cli-assets.heroku.com/install.sh | sh
 
+/usr/local/bin/heroku container:login
 /usr/local/bin/heroku container:push web -a microservice-deploy
 /usr/local/bin/heroku container:release web -a microservice-deploy
